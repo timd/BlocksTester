e0b69a2f73cb239287bc0606769725970e09be7d
Tim Duckett <tim@timduckett.co.uk>
2014-08-21 08:39:01 +0200

    initial commit

--- BlocksTester/ViewController.m
+++ BlocksTester/ViewController.m
@@ -10,18 +10,137 @@
 
 @interface ViewController ()
 
+// Define a block property taking no params
+@property (nonatomic, copy) void (^superSimpleBlock)();
+
+// Define a block property taking a single param
+@property (nonatomic, copy) void (^stringBlock)(NSString*);
+
+// Define a block property taking multiple params
+@property (nonatomic, copy) BOOL (^arrayBlock)(NSArray*, NSString*);
+
 @end
 
 @implementation ViewController
-            
+
+// Declare a block as a typedef that will be
+// defined later
+typedef void (^RemoteDeffedBlock)();
+
+// Declare and implement typedeffed block
+typedef void (^TypeDeffedBlock)();
+
+// Implement the typedeff'd block
+TypeDeffedBlock typeDeffedBlock = ^void() {
+    NSLog(@"This is a typed block!");
+};
+
+
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    
+    
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
+
+- (IBAction)didTapBlockButton:(id)sender {
+
+    // ****************************************************************
+    // Define the block inline and use it with the really simple method
+    // ****************************************************************
+
+    void (^simpleBlock)() = ^void() {
+        NSLog(@"this is a simple block!");
+    };
+    
+    [self doSomethingWithReallySimpleBlock:simpleBlock];
+
+    // *****************************************
+    // Same thing as above with a block property
+    // *****************************************
+    
+    self.superSimpleBlock = ^void() {
+        NSLog(@"this is a super simple block!");
+    };
+    
+    [self doSomethingWithReallySimpleBlock:self.superSimpleBlock];
+
+    // **************************************************************************
+    // Call the simple block method with a block that was defined using a typedef
+    // **************************************************************************
+    
+    [self doSomethingWithReallySimpleBlock:typeDeffedBlock];
+
+    // *************************************************************************************
+    // Call the simple block method with a block that was defined using a typedef elsewhere,
+    // but defined locally
+    // *************************************************************************************
+    
+    RemoteDeffedBlock remoteDeffedBlock = ^void() {
+        NSLog(@"This is a remotely-deffed typed block!");
+    };
+    
+    [self doSomethingWithReallySimpleBlock:remoteDeffedBlock];
+    
+
+    // **********************************************************************
+    // Call the simple block method with a block property that's defined here
+    // **********************************************************************
+    
+    self.stringBlock = ^void(NSString *theText) {
+        NSLog(@"did this from within a block, with text %@", theText);
+    };
+
+    [self doSomethingWithSimpleBlock:self.stringBlock];
+
+    
+    // ***************************************************************************
+    // Call the betterBlock method with a block defined locally to take parameters
+    // ***************************************************************************
+
+    self.arrayBlock = ^BOOL(NSArray *theArray, NSString *theString) {
+        NSLog(@"The string is %@", theString);
+        for (id thing in theArray) {
+            NSLog(@"thing = %@", thing);
+        }
+        return YES;
+    };
+    
+    [self doSomethingWithBetterBlock:self.arrayBlock];
+    
+}
+
+-(void)doSomethingWithReallySimpleBlock:(void (^)())theBlock {
+    
+    // Simplest possible way of executing the block
+    theBlock();
+}
+
+- (void)doSomethingWithSimpleBlock:(void (^)(NSString*))theBlock {
+    
+    // Passing in some local data into the block
+    NSString *foo = @"some more text";
+    theBlock(foo);
+}
+
+-(void)doSomethingWithBetterBlock:(BOOL (^)(NSArray*, NSString*))betterBlock {
+    
+    // More complex local data
+    NSArray *array = @[@"one", @"two", @"three", @"four", @"five"];
+    NSString *string = @"foo";
+    
+    // And handling a return type
+    BOOL blockReturn = betterBlock(array, string);
+    
+    NSLog(@"Result of block = %d", blockReturn);
+    
+}
+
+
 
 @end
--- README.md
+++ README.md
@@ -2,3 +2,5 @@ BlocksTester
 ============
 
 Being a small project as practice in remembering the infinite variety of blocks syntax.
+
+Thanks (fuckingblocksyntax.com)[http://fuckingblocksyntax.com]!
